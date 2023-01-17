ca23659e2feda0bcdf239da0f9dd0343993e3b9f
Q <thanphap0912@gmail.com>
2023-01-17 22:11:58 +0700

    fix config

--- src/config/index.js
+++ src/config/index.js
@@ -6,7 +6,7 @@ const config = {
 
     DB_USER: process.env.DB_USER,
     DB_PASSWORD: process.env.DB_PASSWORD,
-    BD_HOST: process.env.DB_HOST,
+    DB_HOST: process.env.DB_HOST,
     DB_PORT: process.env.DB_PORT,
     DB_DIALECT: process.env.DB_DIALECT,
     DB_NAME: process.env.DB_NAME,
